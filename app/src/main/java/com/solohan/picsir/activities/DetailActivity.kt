0228dbde63c175f331248688f8951abf76cb3308
hansol <wkdqhrh00@naver.com>
2018-11-04 00:55:17 +0900

    181104 새벽 : permission 문제있어 permission 관리 추가

--- app/src/main/java/com/solohan/picsir/activities/DetailActivity.kt
+++ app/src/main/java/com/solohan/picsir/activities/DetailActivity.kt
@@ -1,15 +1,12 @@
 package com.solohan.picsir.activities
 
 import android.Manifest
-import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.util.Log
 import android.view.View
